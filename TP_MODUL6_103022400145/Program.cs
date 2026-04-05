cc1602c68099417c4b6aba41443e23219a26eb6d
mrezasaputra <mrezagithub@gmail.com>
2026-04-06 05:20:36 +0700

    SayaMusicTrack commit

--- TP_MODUL6_103022400145/Program.cs
+++ TP_MODUL6_103022400145/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class SayaMusicTrack
+{
+    private int id;
+    private string playCount;
+    private string title;
+
+    public SayaMusicTrack(string title)
+    {
+        this.title = title;
+        this.playCount = "0";
+
+        // Bikin ID random 5 digit
+        Random rnd = new Random();
+        this.id = rnd.Next(10000, 100000);
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        int tempPlayCount = int.Parse(this.playCount);
+        tempPlayCount += count;
+
+        this.playCount = tempPlayCount.ToString();
+    }
+
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine($"ID Lagu: {this.id}");
+        Console.WriteLine($"Judul Lagu: {this.title}");
+        Console.WriteLine($"Total Play: {this.playCount}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack lagu = new SayaMusicTrack("Mati-Matian - Koma");
+
+        Console.WriteLine("- Data Awal");
+        lagu.PrintTrackDetails();
+
+        Console.WriteLine("\n- Setelah Tambah Play Count");
+        lagu.IncreasePlayCount(150);
+        lagu.PrintTrackDetails();
+        Console.ReadLine();
+    }
+}
